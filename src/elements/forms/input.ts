b6b7822cdb555ace2d92feba583289cc1934fdc7
Dylan Britz <britzdylan@gmail.com>
2023-05-07 12:01:56 +0200

    Input Upgrades

--- src/elements/forms/input.ts
+++ src/elements/forms/input.ts
@@ -1,102 +1,150 @@
 const input = (theme: (arg0: string) => string | number) => {
+  const label = {
+    width: '100%',
+    fontSize: theme('fontSize.label-sm'),
+    fontWeight: theme('fontWeight.bold'),
+  };
+  const inputSpan = {
+    backgroundColor: '#fff',
+    overflow: 'hidden',
+    padding: 0,
+    width: '100%',
+    fontSize: theme('fontSize.label-sm'),
+    fontWeight: theme('fontWeight.bold'),
+    display: 'flex',
+    alignItems: 'center',
+    gap: theme('spacing.2'),
+    justifyContent: 'space-between',
+    borderRadius: theme('borderRadius.md'),
+    border: `1px solid ${theme('colors.gray.300')}`,
+    '&:focus-within': {
+      outlineStyle: 'solid',
+      outlineColor: theme('colors.blue.200'),
+      outlineWidth: '2px',
+      outlineOffset: '0px',
+      border: `1px solid ${theme('colors.blue.300')}`,
+    },
+    '& .icon, svg ': {
+      fontSize: theme('spacing.5'),
+      height: theme('spacing.5'),
+    },
+    '& .icon:first-child': {
+      marginLeft: theme('spacing.2'),
+    },
+    '& .icon:last-child': {
+      marginRight: theme('spacing.2'),
+    },
+    '& svg:first-child': {
+      marginLeft: theme('spacing.2'),
+    },
+    '& svg:last-child': {
+      marginRight: theme('spacing.2'),
+    },
+    '&.error': {
+      border: `1px solid ${theme('colors.red.500')}`,
+      outlineStyle: 'solid',
+      outlineColor: theme('colors.red.300'),
+      outlineWidth: '2px',
+    },
+    '&.success': {
+      border: `1px solid ${theme('colors.green.500')}`,
+      outlineStyle: 'solid',
+      outlineColor: theme('colors.green.300'),
+      outlineWidth: '2px',
+    },
+  };
+  const prefixSuffix = {
+    color: theme('colors.gray.500'),
+    backgroundColor: theme('colors.gray.50'),
+    fontSize: theme('fontSize.label-sm'),
+    fontWeight: theme('fontWeight.normal'),
+    display: 'flex',
+    alignItems: 'center',
+    gap: theme('spacing.2'),
+    justifyContent: 'center',
+    height: '100%',
+    padding: `${theme('spacing[1.5]')} ${theme('spacing.3')}`,
+  };
+  const input = {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    overflow: 'hidden',
+    padding: 2,
+    gap: theme('spacing.1'),
+  };
+  const inputReset = {
+    width: '100%',
+    padding: `${theme('spacing[1.5]')} ${theme('spacing.3')}`,
+    fontSize: theme('fontSize.label-sm'),
+    fontWeight: theme('fontWeight.normal'),
+    '&:focus': {
+      outline: 'none',
+      border: `none`,
+    },
+    '&:disabled': {
+      PointerEvents: 'none',
+      outline: 'none',
+      border: `none`,
+    },
+    '&:active': {
+      outline: 'none',
+      border: `none`,
+    },
+    '&:hover': {
+      outline: 'none',
+      border: `none`,
+    },
+  };
+
+  const helper = {
+    width: '100%',
+    marginBottom: theme('spacing.1'),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    '& span': {
+      fontSize: theme('fontSize.label-sm'),
+      fontWeight: theme('fontWeight.normal'),
+    },
+  };
+
   return {
     '.input': {
-      width: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'flex-start',
-      justifyContent: 'flex-start',
-      overflow: 'hidden',
-      padding: 4,
+      ...input,
       '& label.label': {
-        width: '100%',
-        fontSize: theme('fontSize.label-sm'),
-        fontWeight: theme('fontWeight.bold'),
-        marginBottom: theme('spacing.1'),
+        ...label,
       },
       '& > span:not(.input-prefix, .input-suffix)': {
-        backgroundColor: '#fff',
-        overflow: 'hidden',
-        padding: 0,
-        width: '100%',
-        height: '100%',
-        fontSize: theme('fontSize.label-sm'),
-        fontWeight: theme('fontWeight.bold'),
-        marginBottom: theme('spacing.1'),
-        display: 'flex',
-        alignItems: 'center',
-        gap: theme('spacing.2'),
-        justifyContent: 'space-between',
-        borderRadius: theme('borderRadius.md'),
-        border: `1px solid ${theme('colors.gray.300')}`,
-        '&:focus-within': {
-          outlineStyle: 'solid',
-          outlineColor: theme('colors.blue.200'),
-          outlineWidth: '2px',
-          outlineOffset: '0px',
-          border: `1px solid ${theme('colors.blue.300')}`,
-        },
-        '& .icon, svg ': {
-          fontSize: theme('spacing.5'),
-          height: theme('spacing.5'),
-        },
-        '& .icon:first-child': {
-          marginLeft: theme('spacing.2'),
-        },
-        '& .icon:last-child': {
-          marginRight: theme('spacing.2'),
-        },
-        '& svg:first-child': {
-          marginLeft: theme('spacing.2'),
-        },
-        '& svg:last-child': {
-          marginRight: theme('spacing.2'),
-        },
-        '&.error': {
-          border: `1px solid ${theme('colors.red.500')}`,
-          outlineStyle: 'solid',
-          outlineColor: theme('colors.red.300'),
-          outlineWidth: '2px',
-        },
-        '&.success': {
-          border: `1px solid ${theme('colors.green.500')}`,
-          outlineStyle: 'solid',
-          outlineColor: theme('colors.green.300'),
-          outlineWidth: '2px',
-        },
+        ...inputSpan,
       },
       '& label.helper': {
-        width: '100%',
-        marginBottom: theme('spacing.1'),
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        '& span': {
-          fontSize: theme('fontSize.label-sm'),
-          fontWeight: theme('fontWeight.normal'),
-        },
+        ...helper,
       },
       '& input': {
-        width: '100%',
-        padding: `${theme('spacing.2')} ${theme('spacing.4')}`,
-        fontSize: theme('fontSize.label-sm'),
-        fontWeight: theme('fontWeight.normal'),
-        '&:focus': {
-          outline: 'none',
-          border: `none`,
+        ...inputReset,
+      },
+
+      '&-large': {
+        '& label.label': {
+          fontSize: theme('fontSize.label'),
         },
-        '&:disabled': {
-          PointerEvents: 'none',
-          outline: 'none',
-          border: `none`,
+        '& > span:not(.input-prefix, .input-suffix)': {
+          fontSize: theme('fontSize.label'),
         },
-        '&:active': {
-          outline: 'none',
-          border: `none`,
+        '& input': {
+          fontSize: theme('fontSize.label'),
+          padding: `${theme('spacing.2')} ${theme('spacing.4')}`,
         },
-        '&:hover': {
-          outline: 'none',
-          border: `none`,
+        '& span.input-prefix': {
+          padding: `${theme('spacing.2')} ${theme('spacing.4')}`,
+          borderRight: `1px solid ${theme('colors.gray.300')}`,
+        },
+        '& span.input-suffix': {
+          padding: `${theme('spacing.2')} ${theme('spacing.4')}`,
+          borderLeft: `1px solid ${theme('colors.gray.300')}`,
         },
       },
       '& > select.input-select': {
@@ -123,29 +171,11 @@ const input = (theme: (arg0: string) => string | number) => {
         },
       },
       '& span.input-prefix': {
-        color: theme('colors.gray.500'),
-        backgroundColor: theme('colors.gray.50'),
-        fontSize: theme('fontSize.label-sm'),
-        fontWeight: theme('fontWeight.normal'),
-        display: 'flex',
-        alignItems: 'center',
-        gap: theme('spacing.2'),
-        justifyContent: 'center',
-        height: '100%',
-        padding: `${theme('spacing.2')} ${theme('spacing.4')}`,
+        ...prefixSuffix,
         borderRight: `1px solid ${theme('colors.gray.300')}`,
       },
       '& span.input-suffix': {
-        color: theme('colors.gray.500'),
-        backgroundColor: theme('colors.gray.50'),
-        fontSize: theme('fontSize.label-sm'),
-        fontWeight: theme('fontWeight.normal'),
-        display: 'flex',
-        alignItems: 'center',
-        gap: theme('spacing.2'),
-        justifyContent: 'center',
-        height: '100%',
-        padding: `${theme('spacing.2')} ${theme('spacing.4')}`,
+        ...prefixSuffix,
         borderLeft: `1px solid ${theme('colors.gray.300')}`,
       },
     },
