c65e104d8302d260c3d398d23c21f843708b9abd
Dylan Britz <britzdylan@gmail.com>
2023-05-09 16:41:58 +0200

    Avatar

--- src/elements/design-patterns/avatar.ts
+++ src/elements/design-patterns/avatar.ts
@@ -1,4 +1,13 @@
 const avatar = (theme: (arg0: string) => string | number) => {
+  const status = {
+    position: 'absolute',
+    top: -5,
+    right: 5,
+    width: theme('spacing.3'),
+    height: theme('spacing.3'),
+    borderRadius: '9999px',
+    border: `1px solid ${theme('colors.gray.50')}`,
+  };
   return {
     '.avatar': {
       display: 'inline-flex',
@@ -6,10 +15,19 @@ const avatar = (theme: (arg0: string) => string | number) => {
       justifyContent: 'center',
       width: theme('spacing.12'),
       height: theme('spacing.12'),
-      overflow: 'hidden',
       borderRadius: theme('borderRadius.md'),
+      position: 'relative',
+      '& img': {
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        borderRadius: theme('borderRadius.md'),
+      },
       '&-rounded': {
         borderRadius: '9999px',
+        '& img': {
+          borderRadius: '9999px',
+        },
       },
       '&-placeholder': {
         backgroundColor: theme('colors.primary.500'),
@@ -21,11 +39,7 @@ const avatar = (theme: (arg0: string) => string | number) => {
         border: '2px solid #fff',
         boxShadow: theme('boxShadow.light-200'),
       },
-      '& img': {
-        width: '100%',
-        height: '100%',
-        objectFit: 'cover',
-      },
+
       '& svg': {
         width: theme('spacing.5'),
         height: theme('spacing.5'),
@@ -33,6 +47,23 @@ const avatar = (theme: (arg0: string) => string | number) => {
       '& .icon': {
         width: theme('spacing.5'),
         height: theme('spacing.5'),
+      },
+      '& .status': {
+        ...status,
+      },
+      '& .status-green': {
+        ...status,
+        backgroundColor: theme('colors.green.500'),
+      },
+
+      '& .status-yellow': {
+        ...status,
+        backgroundColor: theme('colors.yellow.500'),
+      },
+
+      '& .status-red': {
+        ...status,
+        backgroundColor: theme('colors.red.500'),
       },
     },
   };
