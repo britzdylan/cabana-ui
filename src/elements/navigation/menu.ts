a756a4ee5d3ac95035fd48ce820d5024df722b9f
Dylan Britz <britzdylan@gmail.com>
2023-05-29 20:19:35 +0200

    Menu Upgraded

--- src/elements/navigation/menu.ts
+++ src/elements/navigation/menu.ts
@@ -1,13 +1,67 @@
+import createVariants from '../../lib/colorVariants';
+import directives from '../../lib/directives';
+const { getRadius, iconSize } = directives;
+const defaultColor = 'neutral';
+const defaultRadius = '1';
+
 const menu = (theme: (arg0: string) => string | number) => {
-  const menu = {
+  const base = {
     listStyle: 'none',
     display: 'flex',
     gap: theme('spacing.2'),
     alignItems: 'center',
     justifyContent: 'flex-start',
     '&-vertical': {
+      width: '100%',
       flexDirection: 'column',
+      gap: theme('spacing.3'),
+      alignItems: 'flex-start',
+      '& li': {
+        width: '100%',
+        '& a': {
+          width: '100%',
+        },
+      },
     },
+  };
+
+  const listItemColors = (color: string) => {
+    return {
+      color: theme(`colors.${color}.500`),
+      '&:focus': {
+        color: theme(`colors.${color}.900`),
+      },
+      '&focus-visible': {
+        color: theme(`colors.${color}.900`),
+      },
+      '&.disabled': {
+        color: theme(`colors.${color}.200`),
+        PointerEvents: 'none',
+        cursor: 'default',
+        backgroundColor: 'transparent',
+      },
+      '&:hover': {
+        color: theme(`colors.${color}.800`),
+      },
+      '&:active': {
+        color: theme(`colors.${color}.800`),
+      },
+      '&:visited': {
+        color: theme(`colors.${color}.700`),
+      },
+      '&.active': {
+        color: theme(`colors.${color}.800`),
+        fontWeight: theme('fontWeight.semibold'),
+      },
+    };
+  };
+
+  const listItemAltColors = (color: string) => {
+    return {
+      '& li': {
+        ...listItemColors(color),
+      },
+    };
   };
 
   const listItem = {
@@ -19,90 +73,108 @@ const menu = (theme: (arg0: string) => string | number) => {
     cursor: 'pointer',
     position: 'relative',
     fontSize: theme('fontSize.label-sm'),
-    color: theme('colors.gray.600'),
-    borderRadius: theme('spacing.1'),
-  };
-
-  const setColorShade = (color: number) => {
-    return {
-      color: theme(`colors.gray.${color}`),
-    };
-  };
-
-  const icon = {
-    width: theme('spacing.4'),
-    height: theme('spacing.4'),
+    ...getRadius(theme, defaultRadius),
+    ...listItemColors(defaultColor),
   };
 
   const containedItem = {
-    color: '#fff',
-    fontWeight: theme('fontWeight.semibold'),
-    background: '#000',
+    '&:hover': {
+      color: theme(`colors.${defaultColor}.500`),
+      backgroundColor: theme(`colors.${defaultColor}.100`),
+    },
+    '&.active': {
+      color: '#fff',
+      fontWeight: theme('fontWeight.semibold'),
+      background: theme(`colors.${defaultColor}.800`),
+      '& a': {
+        background: 'transparent',
+      },
+      '&:hover': {
+        background: theme(`colors.${defaultColor}.800`),
+        color: '#fff',
+      },
+    },
+    '&.disabled': {
+      color: theme(`colors.${defaultColor}.200`),
+      PointerEvents: 'none',
+      cursor: 'default',
+      backgroundColor: 'transparent',
+    },
+  };
+
+  const containedItemColors = (color: string) => {
+    return {
+      '& li': {
+        ...listItemColors(color),
+        '&:hover': {
+          backgroundColor: theme(`colors.${color}.100`),
+          color: theme(`colors.${color}.500`),
+        },
+        '&.active': {
+          background: theme(`colors.${color}.800`),
+          '&:hover': {
+            background: theme(`colors.${color}.800`),
+            color: '#fff',
+          },
+        },
+        '&.disabled': {
+          color: theme(`colors.${color}.200`),
+          PointerEvents: 'none',
+          cursor: 'default',
+          backgroundColor: 'transparent',
+        },
+      },
+    };
+  };
+
+  const menu = {
+    ...base,
+    '& li': {
+      ...listItem,
+      '& .icon': {
+        ...iconSize(theme, 4),
+      },
+      '& svg': {
+        ...iconSize(theme, 4),
+      },
+      '& a': {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: theme('spacing.2'),
+        padding: 0,
+        textDecoration: 'none',
+        color: 'inherit',
+        '& .icon': {
+          ...iconSize(theme, 4),
+        },
+        '& svg': {
+          ...iconSize(theme, 4),
+        },
+      },
+    },
+    ...Object.fromEntries(createVariants(listItemAltColors)),
+    '&-contained': {
+      '& li': {
+        ...containedItem,
+      },
+      '&-rounded': {
+        '& li': {
+          ...containedItem,
+          borderRadius: '9999px',
+          padding: `${theme('spacing[1.5]')} ${theme('spacing.3')}`,
+        },
+      },
+      ...Object.fromEntries(createVariants(containedItemColors)),
+    },
   };
 
   return {
     'ul.menu': {
       ...menu,
-      '& li': {
-        ...listItem,
-        '&:focus': {
-          ...setColorShade(800),
-        },
-        '&focus-visible': {
-          ...setColorShade(800),
-        },
-        '&:disabled': {
-          ...setColorShade(200),
-          PointerEvents: 'none',
-        },
-        '& .icon': {
-          ...icon,
-        },
-        '& svg': {
-          ...icon,
-        },
-        '& a': {
-          ...listItem,
-          padding: 0,
-          textDecoration: 'none',
-          '& .icon': {
-            ...icon,
-          },
-          '& svg': {
-            ...icon,
-          },
-          '&:disabled': {
-            ...setColorShade(200),
-            PointerEvents: 'none',
-          },
-        },
-      },
-
-      '& li.active': {
-        color: theme('colors.gray.900'),
-        fontWeight: theme('fontWeight.semibold'),
-      },
-
-      '&-contained': {
-        '& li.active': {
-          ...containedItem,
-          '& a': {
-            ...containedItem,
-          },
-        },
-        '&-rounded': {
-          '& li': {
-            borderRadius: '9999px',
-            padding: `${theme('spacing[1.5]')} ${theme('spacing.3')}`,
-          },
-          '& li.active': {
-            ...containedItem,
-            '& a': {
-              ...containedItem,
-            },
-          },
-        },
-      },
+    },
+    'ol.menu': {
+      ...menu,
     },
   };
 };
