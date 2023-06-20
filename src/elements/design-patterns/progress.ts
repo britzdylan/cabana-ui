5e0f59f83d6fa9be9583f52a9355508610297e9a
Dylan Britz <britzdylan@gmail.com>
2023-06-20 17:06:37 +0200

    feat:added progress component

--- src/elements/design-patterns/progress.ts
+++ src/elements/design-patterns/progress.ts
@@ -1,6 +1,25 @@
+const defaultColor = 'secondary';
+import createVariants from '../../lib/colorVariants';
+
 const progress = (theme: (arg0: string) => string | number) => {
-  const valueColor = (color: string) => {
-    return { backgroundColor: theme(`colors.${color}.500`) };
+  const colors = (color: string) => {
+    return {
+      color: theme(`colors.${color}.500`),
+      backgroundColor: theme(`colors.${color}.500`),
+      '&::-webkit-progress-bar': {
+        backgroundColor: theme(`colors.${color}.200`),
+      },
+
+      /* Change the appearance of the progress bar fill */
+      '&::-webkit-progress-value': {
+        backgroundColor: theme(`colors.${color}.500`),
+      },
+
+      /* Style the progress bar in Firefox */
+      '&::-moz-progress-bar': {
+        backgroundColor: theme(`colors.${color}.500`),
+      },
+    };
   };
   return {
     'progress.progress': {
@@ -8,69 +27,25 @@ const progress = (theme: (arg0: string) => string | number) => {
       /* Set the width of the progress bar */
       width: '100%',
       /* Set the color of the progress bar */
-      color: theme('colors.primary.500'),
+      color: theme(`colors.${defaultColor}.500`),
       borderRadius: theme('borderRadius.full'),
       overflow: 'hidden',
       /* Change the appearance of the progress bar track */
 
       '&::-webkit-progress-bar': {
-        backgroundColor: theme('colors.gray.200'),
+        backgroundColor: theme(`colors.${defaultColor}.200`),
       },
 
       /* Change the appearance of the progress bar fill */
       '&::-webkit-progress-value': {
-        backgroundColor: theme('colors.primary.500'),
+        backgroundColor: theme(`colors.${defaultColor}.500`),
       },
 
       /* Style the progress bar in Firefox */
       '&::-moz-progress-bar': {
-        backgroundColor: theme('colors.primary.500'),
+        backgroundColor: theme(`colors.${defaultColor}.500`),
       },
-
-      '&-secondary': {
-        '&::-webkit-progress-value': {
-          ...valueColor('secondary'),
-        },
-        '&::-moz-progress-bar': {
-          ...valueColor('secondary'),
-        },
-      },
-
-      '&-tertiary': {
-        '&::-webkit-progress-value': {
-          ...valueColor('tertiary'),
-        },
-        '&::-moz-progress-bar': {
-          ...valueColor('tertiary'),
-        },
-      },
-
-      '&-success': {
-        '&::-webkit-progress-value': {
-          ...valueColor('green'),
-        },
-        '&::-moz-progress-bar': {
-          ...valueColor('green'),
-        },
-      },
-
-      '&-error': {
-        '&::-webkit-progress-value': {
-          ...valueColor('red'),
-        },
-        '&::-moz-progress-bar': {
-          ...valueColor('red'),
-        },
-      },
-
-      '&-warning': {
-        '&::-webkit-progress-value': {
-          ...valueColor('yellow'),
-        },
-        '&::-moz-progress-bar': {
-          ...valueColor('yellow'),
-        },
-      },
+      ...Object.fromEntries(createVariants(colors)),
     },
   };
 };
