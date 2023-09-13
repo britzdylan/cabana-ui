29ee6446442d132d7900e323e702662b80522dee
Dylan Britz <britzdylan@gmail.com>
2023-09-13 21:11:44 +0200

    Removed darkmode

--- docs/tailwind.config.cjs
+++ docs/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'media', // or 'media' or 'class
+  darkMode: 'class', // or 'media' or 'class
   theme: {
     extend: {},
   },
